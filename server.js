5706956e4b35a07a337c74d5546e51a3365ee184
DevSamuel06 <samuelribeiro.filho16@gmail.com>
2024-05-17 15:37:17 -0300

    Finalizando login e lógica de login

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const sql = require('mssql');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +22,48 @@ app.use(express.json());
 
 // Servir arquivos estáticos (como index.html)
 app.use(express.static(path.join(__dirname)));
+
+// Rota para cadastrar um novo usuário
+app.post('/register', async (req, res) => {
+    const { nome, email, idade, senha } = req.body;
+
+    try {
+        await sql.connect(config);
+        const hashedPassword = await bcrypt.hash(senha, 10);
+        const request = new sql.Request();
+        await request.query(`
+            INSERT INTO Usuarios (Nome, Email, Idade, Senha)
+            VALUES ('${nome}', '${email}', ${idade}, '${hashedPassword}');
+        `);
+        res.status(201).send('Usuário cadastrado com sucesso.');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao cadastrar o usuário.');
+    }
+});
+
+// Rota para login de usuário
+app.post('/login', async (req, res) => {
+    const { nome, senha } = req.body;
+
+    try {
+        await sql.connect(config);
+        const request = new sql.Request();
+        const result = await request.query(`SELECT * FROM Usuarios WHERE Nome = '${nome}'`);
+        const user = result.recordset[0];
+
+        if (user && await bcrypt.compare(senha, user.Senha)) {
+            const token = jwt.sign({ id: user.Id }, 'secretKey', { expiresIn: '1h' });
+            res.json({ token });
+        } else {
+            res.status(401).send('Credenciais inválidas.');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro ao fazer login.');
+    }
+});
+
 
 // Rota para atualizar a vida do herói e do vilão
 app.post('/atualizarVida', async (req, res) => {
@@ -67,7 +111,11 @@ app.get('/characters', async (req, res) => {
 
 // Rota para servir o arquivo HTML principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('jogo', (req, res) => {
+    res.sendFile(path.join(__dirname, 'jogo.html'));
 });
 
 app.get('/dashboard', (req, res) => {
